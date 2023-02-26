74e7dc127a15b4c329e757466c6c7426af38bccd
Arturs Šefers <sefers.arturs@gmail.com>
2023-02-26 18:46:07 +0200

    Update sp-bar bg

--- src/components/sp-bar.ts
+++ src/components/sp-bar.ts
@@ -3,7 +3,7 @@ import { i18n } from "../i18n"
 
 const template = document.createElement("template")
 template.className =
-    "flex flex-row min-width-70px height-19px align-center bg-white border-1 border-radius-3 inactive-children cursor-pointer"
+    "flex flex-row min-width-70px height-19px align-center bg-semi-white border-1 border-radius-3 inactive-children cursor-pointer"
 template.innerHTML = html`
     <span class="px-1 ml-px bg-gray bold border-radius-3">${i18n("sp")}</span>
     <span id="amount" class="px-1 flex-1 text-right bold"></span>
